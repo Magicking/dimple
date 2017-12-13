71410629bd06367b3dd534ef24b7f0f51b1d060a
Magicking <s@6120.eu>
2017-12-13 21:38:05 +0100

    Add reCaptcha

--- front/js/index.js
+++ front/js/index.js
@@ -1,15 +1,10 @@
 const angular = require('angular')
+const $ = require('jquery')
 const units = require('ethereumjs-units')
 
 const endpoint = 'api'
 
-const sitekey = '6LcezDwUAAAAAKFzR3svWPlSc7n94lSpdu3z3m-t'
-
 window.addEventListener('load', function() {
-  grecaptcha.render('captcha', {
-    'sitekey' : sitekey,
-    'theme' : 'dark'
-  });
   var p = document.getElementById("content-node");
   var p_prime = p.cloneNode(true);
   document.body.appendChild(p_prime);
@@ -19,6 +14,7 @@ ttt = angular.module('dimpleApp', [])
 ttt.controller('dimpleCtl', function DimpleCtrl($scope, $http, $timeout) {
     var line = {txid: "…", addr: "…", amount: "…"}
     $scope.transactions = [line]
+    $scope.canSend = true;
     var updateList = function () {
         $http.get(endpoint + '/list').
           then(function(response) {
@@ -35,7 +31,19 @@ ttt.controller('dimpleCtl', function DimpleCtrl($scope, $http, $timeout) {
         $timeout(updateList, 1000)
     }
     $scope.send = function () {
-        console.log('captcha response: ' + grecaptcha.getResponse());
+        var addr = $('#addr').val()
+        $http.get(endpoint + '/send?to=' + addr,{
+            headers: {
+        'Authorization': grecaptcha.getResponse()
+    }}).
+          then(function(response) {
+              $scope.status = response.status;
+              console.log(response)
+            }, function(response) {
+              console.log(response.data || 'Request failed')
+              $scope.status = response.status
+        });
+        $scope.canSend = false;
     }
     updateList()
 });
