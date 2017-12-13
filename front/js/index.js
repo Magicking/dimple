2f2b01e1e2dfc84d2e554c1592f82cfdee561876
Magicking <s@6120.eu>
2017-12-13 09:06:59 +0100

    Add basic style

---
+++ front/js/index.js
@@ -0,0 +1,39 @@
+const angular = require('angular')
+const units = require('ethereumjs-units')
+
+const endpoint = '/api'
+const sitekey = 'YOUR SITE KEY'
+
+window.addEventListener('load', function() {
+  // Renders the HTML element with id 'example1' as a reCAPTCHA widget.
+  // The id of the reCAPTCHA widget is assigned to 'widgetId1'.
+  grecaptcha.render('captcha', {
+    'sitekey' : sitekey,
+    'theme' : 'dark'
+  });
+});
+
+ttt = angular.module('dimpleApp', [])
+ttt.controller('dimpleCtl', function DimpleCtrl($scope, $http, $timeout) {
+    var line = {txid: "…", addr: "…", amount: "…"}
+    $scope.transactions = [line]
+    var updateList = function () {
+        $http.get(endpoint + '/list').
+          then(function(response) {
+              $scope.status = response.status;
+              $scope.transactions = response.data.map(x => { return {
+                  txid: x.txid,
+                  addr: x.addr,
+                  amount: units.convert(x.amount, 'wei', 'eth') + ' eth'
+              }});
+            }, function(response) {
+              console.log(response.data || 'Request failed')
+              $scope.status = response.status
+        });
+        $timeout(updateList, 1000)
+    }
+    $scope.send = function () {
+        console.log('captcha response: ' + grecaptcha.getResponse());
+    }
+    updateList()
+});
